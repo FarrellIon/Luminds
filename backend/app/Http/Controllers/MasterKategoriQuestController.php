bb3a9c4574392d1862c83dcef1c2fb6ea2f58e29
FarrellIon <104186473+FarrellIon@users.noreply.github.com>
2024-07-02 16:59:02 +0700

    master data kategori quest done

--- backend/app/Http/Controllers/MasterKategoriQuestController.php
+++ backend/app/Http/Controllers/MasterKategoriQuestController.php
@@ -3,8 +3,141 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use App\Models\MasterKategoriQuest;
 
 class MasterKategoriQuestController extends Controller
 {
-    //
+    protected $controller_name = 'kategori_quest';
+
+    public function get(){
+        try {
+            $variable = $this->controller_name;
+            $$variable = MasterKategoriQuest::all();
+
+            $$variable = $$variable->map(function($singular){
+                $singular['encrypted_id'] = Crypt::encryptString($singular->id);
+                $singular['icon'] = Storage::url($singular->icon);
+
+                unset(
+                    $singular['id']
+                );
+
+                return $singular;
+            });
+
+            $response = [
+                'data' => $$variable
+            ];
+
+            return response()->json($response, 200);
+        } catch (\Exception $e) {
+            return response()->json(['errors' => $e->getMessage()], 401);
+        }
+    }
+    
+    public function insert(Request $request){
+        $validator = Validator::make($request->all(),
+            [
+                'nama' => 'required',
+                'icon' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+                'color_code' => 'required',
+            ],
+            [
+                'nama.required' => 'Field nama belum terisi',
+                'icon.required' => 'Field icon belum terisi',
+                'icon.image' => 'Icon harus file gambar',
+                'icon.mimes' => 'File icon harus berkestensi jpeg, png, ataupun jpg',
+                'icon.max' => 'File icon harus berukuran dibawah 2MB',
+                'color_code.required' => 'Field color code belum terisi',
+            ]
+        );
+        
+        if($validator->fails()){
+            return response()->json(['errors' => $validator->errors()->all()], 401);
+        }
+        
+        DB::beginTransaction();
+
+        try {
+            $variable = $this->controller_name;
+
+            //Store image
+            $icon = $request->file('icon');
+            $path = $icon->store('kategori-quest/icons', 'public');
+            
+            $$variable = new MasterKategoriQuest();
+            $$variable->nama = $request->nama;
+            $$variable->icon = $path;
+            $$variable->color_code = $request->color_code;
+            $$variable->input_by = auth()->user()->id;
+            $$variable->save();
+
+            $response = [
+                'message' => 'Berhasil menginput data'
+            ];
+
+            DB::commit();
+            return response()->json($response, 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['errors' => $e->getMessage()], 401);
+        }
+    }
+    
+    public function update(Request $request, $encrypted_id){
+        DB::beginTransaction();
+
+        try {
+            $variable = $this->controller_name;
+            $decrypted_id = Crypt::decryptString($encrypted_id);
+            $$variable = MasterKategoriQuest::findOrFail($decrypted_id);
+
+            //Delete and store image
+            Storage::disk('public')->delete($$variable->icon);
+            $icon = $request->file('icon');
+            $path = $icon->store('kategori-quest/icons', 'public');
+            
+            $$variable->nama = $request->nama;
+            $$variable->icon = $path;
+            $$variable->color_code = $request->color_code;
+            $$variable->input_by = auth()->user()->id;
+            $$variable->save();
+
+            $response = [
+                'message' => 'Berhasil mengupdate data'
+            ];
+
+            DB::commit();
+            return response()->json($response, 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['errors' => $e->getMessage()], 401);
+        }
+    }
+    
+    public function destroy($encrypted_id){
+        DB::beginTransaction();
+
+        try {
+            $variable = $this->controller_name;
+            $decrypted_id = Crypt::decryptString($encrypted_id);
+            $$variable = MasterKategoriQuest::findOrFail($decrypted_id);
+            Storage::disk('public')->delete($$variable->icon);
+            $$variable->delete();
+
+            $response = [
+                'message' => 'Berhasil menghapus data'
+            ];
+
+            DB::commit();
+            return response()->json($response, 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['errors' => $e->getMessage()], 401);
+        }
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\MasterKategoriQuestController;
 
 Route::get('/token', function () {
     return csrf_token(); 
@@ -11,8 +12,12 @@ Route::get('login', [LoginController::class, 'index']);
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::group(['prefix' => 'items', 'middleware' => ['auth:admin']], function () {
-    Route::get('/', [LoginController::class, 'test'])->name('items.test');
-    Route::patch('/{id}', [ItemController::class, 'update'])->name('items.update');
-    Route::delete('/{id}', [ItemController::class, 'destroy'])->name('items.destroy');
+Route::group(['prefix' => 'kategori-quest'], function () {
+    Route::get('/', [MasterKategoriQuestController::class, 'get'])->name('kategori-quest.get');
+
+    Route::group(['middleware' => ['auth:admin']], function () {
+        Route::post('/', [MasterKategoriQuestController::class, 'insert'])->name('kategori-quest.insert');
+        Route::post('/{encrypted_id}', [MasterKategoriQuestController::class, 'update'])->name('kategori-quest.update');
+        Route::delete('/{encrypted_id}', [MasterKategoriQuestController::class, 'destroy'])->name('kategori-quest.destroy');
+    });
 });
