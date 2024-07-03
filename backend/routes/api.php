ce60293718f227a6aa67db434d41acd38712a05f
FarrellIon <104186473+FarrellIon@users.noreply.github.com>
2024-07-03 16:06:17 +0700

    master data kategori rating selesai

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,12 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\MasterKategoriQuestController;
+use App\Http\Controllers\MasterKategoriRatingController;
 
 Route::get('/token', function () {
     return csrf_token(); 
 });
-Route::get('login', [LoginController::class, 'index']);
+Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
@@ -19,5 +21,15 @@ Route::group(['prefix' => 'kategori-quest'], function () {
         Route::post('/', [MasterKategoriQuestController::class, 'insert'])->name('kategori-quest.insert');
         Route::post('/{encrypted_id}', [MasterKategoriQuestController::class, 'update'])->name('kategori-quest.update');
         Route::delete('/{encrypted_id}', [MasterKategoriQuestController::class, 'destroy'])->name('kategori-quest.destroy');
+    });
+});
+
+Route::group(['prefix' => 'kategori-rating'], function () {
+    Route::get('/', [MasterKategoriRatingController::class, 'get'])->name('kategori-rating.get');
+
+    Route::group(['middleware' => ['auth:admin']], function () {
+        Route::post('/', [MasterKategoriRatingController::class, 'insert'])->name('kategori-rating.insert');
+        Route::post('/{encrypted_id}', [MasterKategoriRatingController::class, 'update'])->name('kategori-rating.update');
+        Route::delete('/{encrypted_id}', [MasterKategoriRatingController::class, 'destroy'])->name('kategori-rating.destroy');
     });
 });
